864b6eedfaaf4a368652857d9d665be7a6a0aebb
Min Gyu <tnvnfdla12@gmail.com>
2021-04-18 20:00:43 +0900

    린트 세팅완료

---
+++ app/src/test/java/com/example/loverdiary/ExampleUnitTest.kt
@@ -0,0 +1,10 @@
+package com.example.loverdiary
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
